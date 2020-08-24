375f83e4bba267d7b31440b64e9e686460201d41
Gerardo Zamora <gerardozamora@Gerardos-MacBook-Air.local>
2020-08-23 21:03:53 -0500

    Added help comments to Stock class

--- Work/stock.py
+++ Work/stock.py
@@ -3,13 +3,22 @@
 # Exercise 4.1
 
 class Stock:
+    '''
+    Instance of a stock holding containing name, shares, and price.
+    '''
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
     def sell(self, amount):
+    '''
+    Sell amount of shares.
+    '''
         self.shares -= amount
         
     def cost(self):
+    '''
+    Returns the cost of a stock holding.
+    '''
         return self.shares * self.price
